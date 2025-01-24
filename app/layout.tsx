7302910ac5e50aa446599507e335277b22903223
Christos Porios <christos.porios@gmail.com>
2025-01-24 16:31:21 +0200

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body className={`h-full ${geistSans.variable} ${geistMono.variable} font-sans`}>
         {children}
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
